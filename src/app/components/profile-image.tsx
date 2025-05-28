309c6699967fabe20fbbe26f4240d101a080c178
iamsuteerth <suteerth@gmail.com>
2025-05-28 15:33:25 +0530

    Revert "Fixed Profile Image Bug"
    This reverts commit e2e023709ad40e88d02c281f1438500c568acf2f.
    The issue was change in s3 bucket configuration and not related to this bug.

--- src/app/components/profile-image.tsx
+++ src/app/components/profile-image.tsx
@@ -51,7 +51,7 @@ const ProfileImage = memo(({
   }, []);
 
   const imageUrl = token && user
-    ? `/api/customer/profile-image${localTimestamp ? `?t=${localTimestamp}` : ''}`
+    ? `/api/profile-image${localTimestamp ? `?t=${localTimestamp}` : ''}`
     : null;
 
   if (error || !token || !user || !imageUrl) {
